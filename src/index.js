a5bfee465f267c42e87954f64d84d6c6bd4d82c2
Egor97 <egorbizyaev123@gmail.com>
2019-10-09 04:33:50 +0400

    add task solution

--- src/index.js
+++ src/index.js
@@ -9,6 +9,40 @@ module.exports = function getTimeForEducation(
     knowsProgramming = true,
     config = {family: 4}
     ) {
-      return 0;
+    let fullTimeEducation = 800;
+    let fullTimeEducationWithoutProgrammingSkills = fullTimeEducation + 500;
+    let value = 0;
+
+    if(knowsProgramming == true){
+      if(focus == 'family'){
+        value = fullTimeEducation/config['family'];
+      }else if(focus == 'friends'){
+        value = fullTimeEducation/config['friends'];
+      }else if(focus == 'normal_life'){
+        value = fullTimeEducation/config['normal_life'];
+      }else if(focus == 'carrier'){
+        value = fullTimeEducation/config['carrier'];
+      }else if(focus == 'top_peformance'){
+        value = fullTimeEducation/config['top_peformance'];
+      }else if(focus == 'profession'){
+        value = fullTimeEducation/config['profession'];
+      }
+    }else if(knowsProgramming == false){
+      if(focus == 'family'){
+        value = fullTimeEducationWithoutProgrammingSkills/config['family'];
+      }else if(focus == 'friends'){
+        value = fullTimeEducationWithoutProgrammingSkills/config['friends'];
+      }else if(focus == 'normal_life'){
+        value = fullTimeEducationWithoutProgrammingSkills/config['normal_life'];
+      }else if(focus == 'carrier'){
+        value = fullTimeEducationWithoutProgrammingSkills/config['carrier'];
+      }else if(focus == 'top_peformance'){
+        value = fullTimeEducationWithoutProgrammingSkills/config['top_peformance'];
+      }else if(focus == 'profession'){
+        value = fullTimeEducationWithoutProgrammingSkills/config['profession'];
+      }
+    }
+
+    return Math.ceil(value);
   };
   
